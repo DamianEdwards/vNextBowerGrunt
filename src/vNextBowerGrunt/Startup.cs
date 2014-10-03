ff7ccbfc5a7503735aa083f6d03f4684479a406d
David Fowler <davidfowl@gmail.com>
2014-10-03 00:12:30 -0700

    Made changes to stuff

--- src/vNextBowerGrunt/Startup.cs
+++ src/vNextBowerGrunt/Startup.cs
@@ -8,6 +8,7 @@ namespace vNextBowerGrunt
         public void Configure(IApplicationBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+            app.UseFileServer();
         }
     }
 }
