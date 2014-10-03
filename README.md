47440bb9d5e73d7013aeb6f985b58abf6110683b
DamianEdwards <damian@damianedwards.com>
2014-10-02 23:44:39 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#Sample Bower/Grunt Integration with ASP.NET vNext
+This project contains a demonstration of an approach for integration of Bower and Grunt into an ASP.NET vNext project.
---
+++ src/vNextBowerGrunt/gruntfile.js
@@ -0,0 +1,17 @@
+﻿module.exports = function (grunt) {
+
+    grunt.loadNpmTasks("grunt-bower-task");
+
+    grunt.initConfig({
+        bower: {
+            install: {
+                options: {
+                    targetDir: "wwwroot/lib",
+                    layout: "byComponent",
+                }
+            }
+        }
+    });
+
+    grunt.registerTask("default", ["bower:install"]);
+};
